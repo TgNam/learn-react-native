483ef2bc6d191ffc8b4fb42a1360fdbc26e18171
TgNam <namnguyen.bkict@gmail.com>
2024-11-04 16:29:51 +0700

    sudungArray

--- App.tsx
+++ App.tsx
@@ -1,15 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, ScrollView, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [user, setUser] = useState([
+    { id: 1, name: "Nam1", age: 20 },
+    { id: 2, name: "Nam2", age: 20 },
+    { id: 3, name: "Nam3", age: 20 },
+    { id: 4, name: "Nam4", age: 20 },
+    { id: 5, name: "Nam5", age: 20 },
+    { id: 6, name: "Nam6", age: 20 },
+    { id: 7, name: "Nam7", age: 20 },
+    { id: 8, name: "Nam8", age: 20 },
+    { id: 9, name: "Nam9", age: 20 },
+    { id: 10, name: "Nam10", age: 20 },
+    { id: 11, name: "Nam11", age: 20 },
+  ]);
+
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={styles.hender}>Xin Chao</Text>
-        <Text>Xin Chao</Text>
-      </View>
-      <Text style={styles.cssText}>Hello world!</Text>
-      <Text>Hello world Nguyễn Trường Nam</Text>
+      <ScrollView>
+        {
+          user.map((item, index) => { // Sửa cú pháp ở đây
+            return (
+              <View key={item.id} style={styles.viewUser}>
+                <Text>{item.name}</Text>
+              </View>
+            )
+          })
+        }
+      </ScrollView>
     </View>
   );
 }
@@ -17,19 +37,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 50,
+    paddingHorizontal: 20,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  cssText: {
-    color: "red",
-    fontSize: 60,
-    borderColor: "red",
-    borderWidth: 1,
-    padding: 20
-  },
-  hender: {
-    color: "green",
-    fontSize: 60
+  viewUser: {
+    padding: 30,
+    backgroundColor: "pink",
+    marginBottom: 30
   }
 });
